95098ad1991e9c3b9f89cca62bf35b0e24f80ae8
iromli <isman.firmansyah@gmail.com>
2018-08-25 02:23:36 +0700

    pass options to wait-for-it.sh

--- scripts/wait-for-it.sh
+++ scripts/wait-for-it.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -f /etc/redhat-release ]; then
-    source scl_source enable python27 && python /opt/scripts/wait_for_it.py
+    source scl_source enable python27 && python /opt/scripts/wait_for_it.py "$@"
 else
-    python /opt/scripts/wait_for_it.py
+    python /opt/scripts/wait_for_it.py "$@"
 fi
