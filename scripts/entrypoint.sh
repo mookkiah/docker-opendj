41938d1125ea54381a6cfbb857e6bce854ca3e76
iromli <isman.firmansyah@gmail.com>
2018-10-30 02:44:21 +0700

    add deploy directory

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,13 +3,13 @@ set -e
 
 mkdir -p /opt/opendj/locks
 
-if [ ! -f /flag/touched ]; then
+if [ ! -f /deploy/touched ]; then
     if [ -f /etc/redhat-release ]; then
         source scl_source enable python27 && python /opt/scripts/entrypoint.py
     else
         python /opt/scripts/entrypoint.py
     fi
-    touch /flag/touched
+    touch /deploy/touched
 fi
 
 exec /opt/opendj/bin/start-ds -N
