1e6bb1a1d29a1a829dc3344ea558fb87649e32eb
iromli <isman.firmansyah@gmail.com>
2018-12-11 22:40:34 +0700

    modify java.properties to disable endpoint identification

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 mkdir -p /opt/opendj/locks
 
+export JAVA_VERSION=$(java -version 2>&1 | awk -F[\"_] 'NR==1{print $2}')
+
 if [ ! -f /touched ]; then
     python /opt/scripts/entrypoint.py
     touch /touched
